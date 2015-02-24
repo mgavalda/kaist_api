513b3c5b85c2cdcb33b8aa9f98d19f7522041e3b
Marsal Gavalda <mgavalda@gmail.com>
2015-02-24 06:32:25 +0000

    Added api call

--- index.php
+++ index.php
@@ -16,6 +16,39 @@ function logc( $data) {
   echo $output;
 }
 
+function call_api($method, $url, $data = false)
+{
+    $curl = curl_init();
+    switch ($method)
+    {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    // Optional Authentication:
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
+}
+
 function startsWith( $haystack, $needle)
 {
   $length = strlen($needle);
@@ -74,6 +107,54 @@ if (startsWith( $endpoint, "double")) {
 }
 
 // other endpoint
+
+else if (startsWith( $endpoint, "mirror")) {
+  
+  if ($method== "GET") {
+     if( isset( $_REQUEST['text'])) {
+       $text = $_REQUEST['text'];    
+       $value = strrev( $text);
+       $result = new stdClass();
+       $result->value = $value;
+       $result_json = json_encode( $result, 128); //JSON_PRETTY_PRINT);
+       echo $result_json;
+     }
+     else {
+       die( "ERROR: Missing parameter 'text' for endpoint 'mirror'");    
+    }   
+  }
+}
+
+else if (startsWith( $endpoint, "geocode")) {
+  
+  if ($method== "GET") {
+     if( isset( $_REQUEST['text'])) {
+       $text = $_REQUEST['text']; 
+         
+       $url = "https://maps.googleapis.com/maps/api/geocode/json?address=" . $text;
+       $response_raw = call_api( "GET", $url);
+       $response_json = json_decode( $response_raw);
+       //$geometry = $value->geometry;
+       $results = $response_json->results;
+       $top_result = $results[ 0];
+       $lat = $top_result->geometry->location->lat;
+       $long = $top_result->geometry->location->lng;
+       $result = new stdClass();
+       $result->lat = $lat;
+       $result->long = $long;
+         
+       
+       $result_json = json_encode( $result, 128); //JSON_PRETTY_PRINT);
+       echo $result_json;
+     }
+     else {
+       die( "ERROR: Missing parameter 'text' for endpoint 'mirror'");    
+    }   
+  }
+}
+
+
+
 else {
   echo "Error: unknown endpoint \"$endpoint\".";
 }
